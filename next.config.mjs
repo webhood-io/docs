62742b789becb7022bd27ce277517f82ca8fe2ee
Markus Lehtonen <28872014+markusleh@users.noreply.github.com>
2023-08-07 19:29:13 +0000

    chore(deps): update dependencies, remove mermaid

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /**
  * @type {import('next').NextConfig}
  */
-import { remarkMermaid } from 'remark-mermaid-nextra';
 import nextra from "nextra";
 
 
@@ -9,7 +8,7 @@ const withNextra = nextra({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
   mdxOptions: {
-    remarkPlugins: [remarkMermaid],
+    remarkPlugins: [],
   }
 })
 
