e5d7508d91cfb58e89cce4b58b4e0a655f9922af
markusleh <28872014+markusleh@users.noreply.github.com>
2023-10-29 16:13:18 +0200

    add proper favicons

--- theme.config.tsx
+++ theme.config.tsx
@@ -56,7 +56,13 @@ const config: DocsThemeConfig = {
   },
   head: (
     <>
-      <link rel="icon" type="image/x-icon" href="/logo/favicon.svg" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/logo/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/logo/favicon-32x32.png"/>
+      <link rel="icon" type="image/png" sizes="16x16" href="/logo/favicon-16x16.png"/>
+      <link rel="manifest" href="/logo/site.webmanifest"/>
+      <link rel="mask-icon" href="/logo/safari-pinned-tab.svg" color="#5bbad5"/>
+      <meta name="msapplication-TileColor" content="#da532c"/>
+      <meta name="theme-color" content="#ffffff"></meta>
     </>
   ),
   useNextSeoProps() {
