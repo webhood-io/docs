31ef6a942923647c592a80cd8b8ecf6a72b6fac2
markusleh <28872014+markusleh@users.noreply.github.com>
2024-02-12 13:25:19 +0200

    add  og:image to meta tags

--- theme.config.tsx
+++ theme.config.tsx
@@ -51,20 +51,40 @@ const config: DocsThemeConfig = {
   head: (
     <>
       <link rel="icon" type="image/svg+xml" href="/logo/logo.svg" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/logo/apple-touch-icon.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/logo/favicon-32x32.png"/>
-      <link rel="icon" type="image/png" sizes="16x16" href="/logo/favicon-16x16.png"/>
-      <link rel="manifest" href="/logo/site.webmanifest"/>
-      <link rel="mask-icon" href="/logo/safari-pinned-tab.svg" color="#5bbad5"/>
-      <meta name="msapplication-TileColor" content="#da532c"/>
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/logo/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/logo/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/logo/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/logo/site.webmanifest" />
+      <link
+        rel="mask-icon"
+        href="/logo/safari-pinned-tab.svg"
+        color="#5bbad5"
+      />
+      <meta name="msapplication-TileColor" content="#da532c" />
       <meta name="theme-color" content="#ffffff"></meta>
+      <meta property="og:image" content="https://www.webhood.io/api/og" />
     </>
   ),
   useNextSeoProps() {
     const { frontMatter } = useConfig()
     return {
       titleTemplate: "%s - Webhood",
-      description: frontMatter.description || "Webhood - Self-hosted URL Scanner",
+      description:
+        frontMatter.description || "Webhood - Self-hosted URL Scanner",
     }
   },
 }
